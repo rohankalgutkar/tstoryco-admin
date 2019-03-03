8c0c975db2a6eb16052a1d4bb390fbabc3e3dec4
Rohan Kalgutkar <hello@rohankalgutkar.in>
2019-03-04 00:08:59 +0530

    Updates for heroku

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 3000
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname+'/index.html'));
   });
 
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || port);
 console.log('Running at Port 3000');
