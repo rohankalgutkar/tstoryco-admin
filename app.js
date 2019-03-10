a24297037a165df9c7a35029168a78140197bb78
Rohan Kalgutkar <hello@rohankalgutkar.in>
2019-03-10 18:35:44 +0530

    View Sales R1 complete

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
 var customAPIs = require('./custom')
+// temp
+const mongoose = require('./mongoose.js')
+
+const hbs = require('hbs');
 
 const app = express()
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+
+hbs.registerHelper('blockquote', function (data, options) {
+    var output = "";
+    console.log('item length ' + data.length)
+
+    for (var i = 0, l = data.length; i < l; i++) {
+      var objRecord = data[i];
+      var date = customAPIs.formatDate(objRecord.date);
+
+        output = output 
+        + '<blockquote> <ul class="alt"> '  
+        + '<li> <span class="icon fa-calendar"> &nbsp;' + date + '</span></li>'
+        + '<li> <span class="icon fa-user-circle-o"> &nbsp;' + objRecord.cust_name + '</span></li>'
+        + '<li> <span class="icon fa-leaf"> &nbsp;' + objRecord.prod_name + '</span></li>'
+        + '<li> <span class="icon fa-money"> &nbsp; ₹' + objRecord.grand_total + '</span></li>'
+        
+        + '</ul> </blockquote>'
+    }
+
+    return output;
+});
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    // var salesData = customAPIs.getAllSales(); console.log('In /, data is: ' +
+    // (salesData)) res.render('index', {   data: salesData });
+
+    var salesMaster = mongoose.Sale;
+    salesMaster.find({}).sort({date_added: -1}).exec((err, docs) => {
+        if (err) {
+            console.log('Unable to connect to the DB!')
+        } else {
+            res.render('index', {data: docs});
+        }
+    });
 });
 
 app.post('/salesentry', function (req, res) {
--- custom.js
+++ custom.js
@@ -1,12 +1,24 @@
 const mongoose = require('./mongoose.js')
 const _ = require('lodash')
 
+const deliveryAmt = 100
+
 var preProcessSalesData = function (formData) {
-    console.log('Form input: ' + JSON.stringify(formData))
-    
+    // console.log('Form input: ' + JSON.stringify(formData))
+
     formData.prod_face = _.has(formData, 'prod_face')
     formData.prod_quote = _.has(formData, 'prod_quote')
     formData.delivery = _.has(formData, 'delivery')
+
+    formData.prod_total_price = _.subtract(formData.prod_price, formData.prod_disc);
+
+    if(formData.delivery){
+        formData.grand_total = _.add(formData.prod_total_price, deliveryAmt)
+    } else {
+        formData.grand_total = formData.prod_total_price
+    }
+
+    formData.date_added = _.now()
 
     return formData
 }
@@ -16,7 +28,39 @@ var insertSale = function (formData) {
     sale.save()
 }
 
+var getAllSales = function () {
+    var salesMaster = mongoose.Sale;
+    var salesData = [];
+    salesMaster.find({}, (err, docs) => {
+            if (err) {
+                console.log('Unable to connect to the DB!')
+            } else {
+                return docs;
+                // console.log('Fetched records: ' + docs);
+            }
+        });
+    
+        // return salesData;
+}
+
+var formatDate = function formatDate(date) {
+    var monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+  
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+  
+    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+  }
+
 module.exports = {
     preProcessSalesData,
-    insertSale
+    insertSale,
+    getAllSales,
+    formatDate
 }
